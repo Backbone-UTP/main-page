1be1a34dc4af3750d91b1d1e6eda34950c8d933b
Djkde01 <sergio.estrella@utp.edu.co>
2023-09-04 21:40:15 -0500

    feat (Redirect to IG)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'static';
+  constructor() {
+    window.location.href = 'https://www.instagram.com/backboneutp/';
+  }
 }
