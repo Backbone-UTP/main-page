2c20a51019590fcd2d16c78d3b2bc9f9e48ea217
Djkde01 <sergio.estrella@utp.edu.co>
2024-07-09 16:32:19 -0500

    chore: Update npm dependencies to latest versions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { HealthComponent } from './pages/health/health.component';
 import { MainComponent } from './pages/main/main.component';
 
