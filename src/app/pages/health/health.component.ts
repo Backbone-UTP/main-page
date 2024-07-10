49d3f05cee7bb94a51b0d69273b480cb3baa0db4
Djkde01 <sergio.estrella@utp.edu.co>
2024-07-10 16:50:38 -0500

    chore: Update stylesheets to use SCSS instead of CSS

--- src/app/pages/health/health.component.ts
+++ src/app/pages/health/health.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-health',
   templateUrl: './health.component.html',
-  styleUrls: ['./health.component.css']
+  styleUrls: ['./health.component.scss'],
 })
 export class HealthComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
