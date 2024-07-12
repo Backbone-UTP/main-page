5a855b8b0ae4a74a29483767241dde25bb1e6229
Djkde01 <sergio.estrella@utp.edu.co>
2024-07-12 13:24:36 -0500

    chore: Add Tailwind CSS configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{html,ts,scss}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
