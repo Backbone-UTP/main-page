481629d43b83ad6ac91c95060eb85ec24d1249a8
Djkde01 <sergio.estrella@utp.edu.co>
2024-07-15 16:48:07 -0500

    refactor: Update header component for SFD 2024 page

--- src/app/pages/events/sfd-2024/components/header/header.component.ts
+++ src/app/pages/events/sfd-2024/components/header/header.component.ts
@@ -1,5 +1,13 @@
 import { NgFor } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { ThemeToggleComponent } from '../../../../../shared/components/theme-toogle/theme.toggle.component';
 import { Header } from 'src/app/interfaces/header.interface';
 
@@ -12,9 +20,62 @@ import { Header } from 'src/app/interfaces/header.interface';
 })
 export class SFDHeaderComponent implements OnInit {
   @Input() navItems: Header[] = [];
-  constructor() {}
+
+  @ViewChild('nav', { static: true }) nav!: ElementRef<HTMLElement>; // Use non-null assertion operator or handle potential null
+  private observer!: IntersectionObserver; // Declare observer, will be initialized in ngOnInit
+
+  constructor(
+    private el: ElementRef<HTMLElement>,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
-    // Initialization code goes here
+    this.initIntersectionObserver();
+  }
+
+  ngAfterViewInit(): void {
+    const sections = this.el.nativeElement.querySelectorAll('main');
+    sections.forEach((section) => {
+      this.observer.observe(section);
+    });
+  }
+
+  private initIntersectionObserver(): void {
+    const callback = (
+      entries: IntersectionObserverEntry[],
+      observer: IntersectionObserver
+    ) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const navItems = this.nav.nativeElement.querySelectorAll('a');
+          navItems.forEach((item) => {
+            const itemId = item.getAttribute('aria-label');
+            if (itemId && itemId === entry.target.id) {
+              this.renderer.addClass(item, 'text-primary');
+            } else {
+              this.renderer.removeClass(item, 'text-primary');
+            }
+          });
+        }
+      });
+    };
+
+    this.observer = new IntersectionObserver(callback, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.3,
+    });
+  }
+
+  @HostListener('document:visibilitychange')
+  onVisibilityChange(): void {
+    if (document.visibilityState === 'hidden') {
+      this.observer.disconnect();
+    } else {
+      const sections = this.el.nativeElement.querySelectorAll('main');
+      sections.forEach((section) => {
+        this.observer.observe(section);
+      });
+    }
   }
 }
