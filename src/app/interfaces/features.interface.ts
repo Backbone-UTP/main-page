48a2348a8f2f7f2d3ce766027a95de982673fca5
Djkde01 <sergio.estrella@utp.edu.co>
2024-07-15 16:48:45 -0500

    feat: Add Feature interface for defining features with heading, content, and icon

---
+++ src/app/interfaces/features.interface.ts
@@ -0,0 +1,7 @@
+import { Icon } from './icons.interface';
+
+export interface Feature {
+  heading: string;
+  content: string;
+  svg: Icon;
+}
---
+++ src/assets/content/sfd-features.ts
@@ -0,0 +1,28 @@
+import { Feature } from 'src/app/interfaces/features.interface';
+
+export const features: Feature[] = [
+  {
+    heading: 'Desarrollo de Software',
+    content:
+      'Inmersión profunda en las últimas tecnologías y metodologías de programación, desarrollo ágil y DevOps. Además, talleres prácticos para aplicando conocimiento en proyectos reales.',
+    svg: 'code',
+  },
+  {
+    heading: 'Comunidad y Filosofía FOSS',
+    content:
+      'Espacio para explorar el impacto del Software Libre y de Código Abierto en la tecnología moderna. Conoce líderes de proyectos FOSS, cómo contribuir a estos proyectos y discutir el futuro del software libre.',
+    svg: 'group',
+  },
+  {
+    heading: 'Investigación e Innovación',
+    content:
+      'Avances más recientes en tecnología y ciencia. Investigadores de renombre compartirán sus descubrimientos y cómo estos pueden aplicarse para resolver problemas complejos.',
+    svg: 'search',
+  },
+  {
+    heading: 'Diseño y Expresión Libre',
+    content:
+      'Creativos de todas las disciplinas descubrirán nuevas formas de expresión a través de herramientas de diseño de código abierto. Explorando cómo el diseño puede influir en la sociedad y la cultura.',
+    svg: 'art',
+  },
+];
