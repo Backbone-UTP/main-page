027ffc094141df1c471267d706e9ac727457240e
Djkde01 <sergio.estrella@utp.edu.co>
2024-07-15 17:54:52 -0500

    feat: Add staff data to SFD 2024 page

---
+++ src/assets/content/sfd.people.ts
@@ -0,0 +1,37 @@
+import { Icon } from 'src/app/interfaces/icons.interface';
+
+export const staff = [
+  {
+    name: 'Paula A. Castro',
+    role: 'Ing. Sistemas, Integrations Developer',
+    image: '/assets/images/staff_1.jpeg',
+    links: [
+      {
+        name: 'linkedin' as Icon,
+        url: 'https://www.linkedin.com/in/paula-a-castro/',
+      },
+      { name: 'github' as Icon, url: 'https://www.twitter.com' },
+    ],
+  },
+  {
+    name: 'Jhon A. Gaviria',
+    role: 'Ing. Sistemas, Backend Developer',
+    image: '/assets/images/staff_2.jpeg',
+    links: [
+      {
+        name: 'linkedin' as Icon,
+        url: 'https://www.linkedin.com/in/jgaviria0/',
+      },
+      { name: 'github' as Icon, url: 'https://github.com/JGaviria0' },
+    ],
+  },
+  {
+    name: 'Sergio Estrella',
+    role: 'Est. Ing. Sistemas, Fullstack Developer',
+    image: '/assets/images/staff_0.jpeg',
+    links: [
+      { name: 'linkedin' as Icon, url: 'https://www.linkedin.com/in/djkde' },
+      { name: 'github' as Icon, url: 'https://github.com/Djkde01' },
+    ],
+  },
+];
