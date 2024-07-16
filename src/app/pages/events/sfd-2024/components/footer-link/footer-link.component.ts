7651f654b43ad72d2899fba9633a46382f7738f7
Djkde01 <sergio.estrella@utp.edu.co>
2024-07-16 02:12:18 -0500

    feat: Add footer-link component for SFD 2024 page

---
+++ src/app/pages/events/sfd-2024/components/footer-link/footer-link.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-link',
+  standalone: true,
+
+  templateUrl: './footer-link.component.html',
+  styleUrls: ['./footer-link.component.scss'],
+})
+export class FooterLinkComponent {
+  @Input() url: string = '';
+}
