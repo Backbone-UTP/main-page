ffd83686205d60f273a807321334c3dbd701bb60
JuanMa216 <juan.rojas6@utp.edu.co>
2025-06-25 18:31:29 -0500

    Implementacion del componente Card, realizado con Angular. Se agregó la carpeta projects-cards con codigo de HTML, SCSS y TS

---
+++ src/app/shared/components/projects-cards/projects.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss'] 
+})
+export class ProjectsComponent {
+  @Input() title!: string;
+  @Input() description!: string;
+  @Input() imageUrl!: string;
+  @Input() status!: string;
+  @Input() link!: string;
+}
