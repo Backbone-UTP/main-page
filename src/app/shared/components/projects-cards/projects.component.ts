41c939c7199f2ca8d01cdf21c89fa0c31993afaf
Djkde01 <sergio.estrella@utp.edu.co>
2025-06-28 10:50:56 -0500

    Enhance ProjectsComponent: improve card layout, add status badge styling, and refine TypeScript inputs

--- src/app/shared/components/projects-cards/projects.component.ts
+++ src/app/shared/components/projects-cards/projects.component.ts
@@ -1,14 +1,32 @@
 import { Component, Input } from '@angular/core';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+} from '@angular/material/card';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss'] 
+  styleUrls: ['./projects.component.scss'],
+  standalone: true,
+  imports: [MatCard, MatCardActions, MatCardContent],
 })
 export class ProjectsComponent {
   @Input() title!: string;
   @Input() description!: string;
   @Input() imageUrl!: string;
-  @Input() status!: string;
+  @Input() status!: 'En Desarrollo' | 'Publicado' | 'Próximamente';
   @Input() link!: string;
+
+  getStatusColor(): string {
+    switch (this.status) {
+      case 'Publicado':
+        return '#00922C'; // Green
+      case 'En Desarrollo':
+        return '#002EA2'; // Blue
+      case 'Próximamente':
+        return '#CA9500'; // Yellow
+    }
+  }
 }
