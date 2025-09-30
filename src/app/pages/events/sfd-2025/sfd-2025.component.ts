67e53e896901e36a244666fa61ea79452087e477
notnohora <nohora.cardozo@utp.edu.co>
2025-09-29 21:28:53 -0500

    nueva estructura de codigo

--- src/app/pages/events/sfd-2025/sfd-2025.component.ts
+++ src/app/pages/events/sfd-2025/sfd-2025.component.ts
@@ -18,6 +18,7 @@ import { AboutEventComponent } from './components/about-event/about-event.compon
 import { GalleryComponent } from '../../../shared/components/gallery/gallery.component';
 import { sfdGalleryImages } from 'src/assets/content/sfd-gallery';
 import {TeamSectionComponent} from "./components/team-section/team-section.component";
+import { SponsorComponent } from "./components/sponsor/sponsor.component";
 
 @Component({
   selector: 'app-sfd-2025',
@@ -33,7 +34,8 @@ import {TeamSectionComponent} from "./components/team-section/team-section.compo
     IconComponent,
     AboutEventComponent,
     GalleryComponent,
-    TeamSectionComponent
+    TeamSectionComponent,
+    SponsorComponent
 ],
   templateUrl: './sfd-2025.component.html',
   styleUrl: './sfd-2025.component.scss',
