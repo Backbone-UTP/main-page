34ed6eaf25345ce58c369eb25151a71852926d78
Djkde01 <sergio.estrella@utp.edu.co>
2026-02-21 12:08:28 -0500

    feat: add Code of Conduct component with HTML, SCSS, and unit tests

---
+++ src/app/pages/code-of-conduct/code-of-conduct.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CodeOfConductComponent } from './code-of-conduct.component';
+
+describe('CodeOfConductComponent', () => {
+  let component: CodeOfConductComponent;
+  let fixture: ComponentFixture<CodeOfConductComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CodeOfConductComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CodeOfConductComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
